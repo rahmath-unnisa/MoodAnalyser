2172116c9baa43887ddcd938166e84abed6d85cf
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-13 00:23:58 +0530

    [Rahmath]Add: Analyzing Invalid Mood Using Exception

--- MoodAnalyzer/Analyzer.cs
+++ MoodAnalyzer/Analyzer.cs
@@ -10,28 +10,43 @@ namespace MoodAnalyzer
         }
         public string AnalyseMood()
         {
-          
             try
             {
-                if (message == null)
-                {
-                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL_MOOD, "Message is Null");
-                }
-                if (message.Equals(""))
-                {
-                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_MOOD, "Message is Empty");
-                }
-
                 if (message.ToLower().Contains("sad"))
                 {
                     return "Sad";
-                }  
+                }
+                else
+                {
+                    return "Happy";
+                }
+                return message;
             }
-            catch(MoodAnalyzerException )
+            catch (Exception)
             {
-                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_MOOD, "Message is Empty"); 
+                return "Happy";
             }
-            return "Happy";
         }
+        //try
+        //{
+        //    if (message == null)
+        //    {
+        //        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL_MOOD, "Message is Null");
+        //    }
+        //    if (message.Equals(""))
+        //    {
+        //        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_MOOD, "Message is Empty");
+        //    }
+
+        //    if (message.ToLower().Contains("sad"))
+        //    {
+        //        return "Sad";
+        //    }
+        //}
+        //catch (MoodAnalyzerException)
+        //{
+        //    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY_MOOD, "Message is Empty");
+        //}
+        //return "Happy";
     }
-}    
+    }
